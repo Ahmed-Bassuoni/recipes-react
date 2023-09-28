f28bcb5ebbfcc07434e62b04c9b047c10b107242
Ahmed Bassuoni <ahmed.m.bass@gmail.com>
2023-09-28 23:30:15 +0300

    Added app header to the app component, and added styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import './App.scss';
 
 import { Route, Routes } from 'react-router-dom';
 import { Router } from './routes/router';
+import { AppHeader } from './components/AppHeader/AppHeader';
 
 function App() {
   return (
-    <div className="">
-      <Routes>
-        {Router.map(({ path, component }) => (
-          <Route key={path} path={path} Component={component} />
-        ))}
-      </Routes>
+    <div className="app">
+      <AppHeader />
+      <div className="container">
+        <Routes>
+          {Router.map(({ path, component }) => (
+            <Route key={path} path={path} Component={component} />
+          ))}
+        </Routes>
+      </div>
     </div>
   );
 }
